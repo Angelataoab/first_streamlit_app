a8cec09a608813df1804d8883ba99b7f569b33d6
Angelataoab <97988359+Angelataoab@users.noreply.github.com>
2023-08-29 09:53:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale Spinach, & Rocket Smoothie')
+streamlit.text('🐔 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-boiled Free-range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
